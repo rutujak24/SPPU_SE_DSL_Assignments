0cfd0403507b06f1984963cfb9547cf68609fe5c
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-01-11 10:52:19 +0530

    Update and rename 4a.cpp to DSL4.cpp

--- DSL4.cpp
+++ DSL4.cpp
@@ -1,10 +1,11 @@
-//============================================================================
-// Name        : 4a.cpp
-// Author      : kandlagunta rohith
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*Department of Computer Engineering has student's club named 'Pinnacle Club'. Students of Second, third and final year of department can be granted membership on request. Similarly one may cancel the membership of club. First node is reserved for president of club and last node is reserved for secretary of club. Write C++ program to maintain club member‘s information using singly linked list. Store student PRN and Name. Write functions to 
+Add and delete the members as well as president or even secretary. 
+Compute total number of members of club
+Display members
+Display list in reverse order using recursion
+Two linked lists exists for two divisions. Concatenate two lists Sort the singly linked list.*/
+
+
 #include<iostream>
 using namespace std;
 
