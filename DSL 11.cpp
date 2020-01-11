55a85e4fa83c9b8cc3ae683d8ba71fea41862698
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-01-11 11:10:09 +0530

    Update DSL 11.cpp

--- DSL 11.cpp
+++ DSL 11.cpp
@@ -1,10 +1,9 @@
-//============================================================================
-// Name        : assignment11.cpp
-// Author      : anuja
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*a)Write C++ program to store roll numbers of student in array who attended training program in random order.
+Write function for searching whether particular student attended training program or not using linear search and sentinel search. 
+
+b) Write C++ program to store roll numbers of student array who attended training program in sorted order. 
+Write function for searching whether particular student attended training program or not using binary search and Fibonacci search.*/
+
 
 #include <iostream>
 using namespace std;
