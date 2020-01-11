f91b6d63af859ee55cae0ba997821ef1062591e7
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-01-11 10:57:00 +0530

    Update DSL 6.cpp

--- DSL 6.cpp
+++ DSL 6.cpp
@@ -1,3 +1,8 @@
+/*Write a C++ program to realize polynomial equation using circular link list and perform following operation
+To input &output polynomials 
+Evaluates a polynomial at given value of x 
+Add two polynomials. */
+
 
 #include<iostream>
 using namespace std;
