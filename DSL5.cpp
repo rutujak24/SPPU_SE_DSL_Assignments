550f55058ba83464164463248865c1695420ebad
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-01-11 10:55:26 +0530

    Update and rename DSL 5.CPP to DSL5.cpp

--- DSL5.cpp
+++ DSL5.cpp
@@ -1,3 +1,11 @@
+/*The ticket booking system of Cinemax theater has to be implemented using C++ program. 
+There are 10 rows and 7 seats in each row. Doubly circular linked list has to be maintained to keep track of free seats at rows.
+Assume some random booking to start with. Use array to store pointers (Head pointer) to each row. On demand 
+The list of available seats is to be displayed 
+The seats are to be booked 
+The booking can be cancelled.*/
+
+
 #include<iostream>
 using namespace std;
 class node
